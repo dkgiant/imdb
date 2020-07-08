e173df1e86a2564efb052860b6a7f0609eb3f81c
dhgiant <dhgiang85@gmail.com>
2020-07-08 20:24:40 +0700

    finish project

--- imdb/spiders/best_movies.py
+++ imdb/spiders/best_movies.py
@@ -6,11 +6,23 @@ from scrapy.spiders import CrawlSpider, Rule
 class BestMoviesSpider(CrawlSpider):
     name = 'best_movies'
     allowed_domains = ['www.imdb.com']
-    start_urls = ['https://www.imdb.com/search/title/?genres=drama&groups=top_250&sort=user_rating,desc']
+    
+    user_agent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://www.imdb.com/search/title/?genres=drama&groups=top_250&sort=user_rating,desc',headers={
+            'User-Agent': self.user_agent
+        })
 
     rules = (
-        Rule(LinkExtractor(restrict_xpaths=r'//h3[@class="lister-item-header"]/a'), callback='parse_item', follow=True),
+        Rule(LinkExtractor(restrict_xpaths=r'//h3[@class="lister-item-header"]/a'), callback='parse_item', follow=True, process_request='set_user_agent'),
+        Rule(LinkExtractor(restrict_xpaths=r'(//a[@class="lister-page-next next-page"])[2]'), process_request='set_user_agent'),
+        
     )
+    def set_user_agent(self, request):
+        request.headers['User-Agent'] = self.user_agent
+
+        return request
 
     def parse_item(self, response):
         yield{
@@ -20,4 +32,5 @@ class BestMoviesSpider(CrawlSpider):
             'genre': response.xpath(r'//div[@class="subtext"]/a/text()').get(),
             'rating': response.xpath(r'//span[@itemprop="ratingValue"]/text()').get(),
             'movie_url': response.url,
+            # 'user-agent':response.request.headers['User-Agent']
         }

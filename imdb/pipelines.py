45529f78144d2e0c48dff425f80b852f82f5c4d1
DESKTOP-MLEK48K\dhgia <dangkynguyhiem@gmail.com>
2020-07-12 09:45:31 +0700

    test pipeline

--- imdb/pipelines.py
+++ imdb/pipelines.py
@@ -5,9 +5,20 @@
 
 
 # useful for handling different item types with a single interface
-from itemadapter import ItemAdapter
+# from itemadapter import ItemAdapter
+import logging
 
 
 class ImdbPipeline:
+    @classmethod
+    def from_crawler(cls, crawler):
+        logging.warning(crawler.settings.get("MONGO_URI"))
+
+    def open_spider(self, spider):
+        logging.warning('SPIDER OPEN FROM PIPEPLINE')
+
+    def close_spider(self, spider):
+        logging.warning('SPIDER CLOSE FROM PIPEPLINE')
+
     def process_item(self, item, spider):
         return item
--- imdb/settings.py
+++ imdb/settings.py
@@ -62,10 +62,10 @@ ROBOTSTXT_OBEY = True
 
 # Configure item pipelines
 # See https://docs.scrapy.org/en/latest/topics/item-pipeline.html
-#ITEM_PIPELINES = {
-#    'imdb.pipelines.ImdbPipeline': 300,
-#}
-
+ITEM_PIPELINES = {
+   'imdb.pipelines.ImdbPipeline': 300,
+}
+MONGO_URI ='HELLO WORLD'
 # Enable and configure the AutoThrottle extension (disabled by default)
 # See https://docs.scrapy.org/en/latest/topics/autothrottle.html
 #AUTOTHROTTLE_ENABLED = True
